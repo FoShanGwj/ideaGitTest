0c18fb84110d25e8599111028d1c6dcbb05b0f32
guoweijie <1546352396@qq.com>
2022-11-17 14:13:22 +0800

    idea first commit

---
+++ src/main/java/com/gwj/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.gwj.git;
+
+/**
+ * 类名：GitTest
+ * 层级：
+ * 作者：guoweijie
+ * 时间：
+ * 业务：
+ * Version: v1.0
+ **/
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
